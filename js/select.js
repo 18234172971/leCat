d5f9fae28689a593a2080512132bdbd628f2e8e1
zhangjian <zhangjian@xiaotong.com>
2018-04-19 23:14:09 +0800

    1.3

---
+++ js/select.js
@@ -0,0 +1,15 @@
+var texiao=document.querySelectorAll(".texiao");
+for(var i=0;i<texiao.length;i++){
+    console.log(texiao[i].classList);
+    if(!(texiao[i].classList==="texiaos")){
+        texiao[i].onclick=function (ev) {
+            console.log(ev);
+            var spans = document.querySelectorAll('.texiao');
+            for (var j = 0; j < spans.length; j++) {
+                spans[j].classList.remove('texiaos');
+            }
+            ev.path[0].classList.add('texiaos');
+        }
+    }
+
+}
