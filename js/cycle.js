e0a5cfe2690362cd4ada4b1b72049d37324d204f
zhangjian <zhangjian@xiaotong.com>
2018-04-21 14:51:40 +0800

    2.7

--- js/cycle.js
+++ js/cycle.js
@@ -1,16 +1,14 @@
-var divs=document.querySelectorAll(".in-button>button");
-var button=document.querySelectorAll('.in-button')[0];
-console.log(button);
+var divs=document.querySelectorAll(".in-button>input");
 for(var i=0;i<divs.length;i++){
-    button.onclick=function (ev) {
-        var target=ev.target;
-        if(!target.classList.contains('divs-active'))
+    divs[i].onclick=function (ev) {
+        console.log(ev.path[0].classList.contains('divs-active'));
+        if(!ev.path[0].classList.contains('divs-active'))
         {
             var active=document.querySelectorAll(".in-button");
             for(var j=0;j<active[0].children.length;j++){
                 active[0].children[j].classList.remove('divs-active');
             }
-            target.classList.add('divs-active');
+            ev.path[0].classList.add('divs-active');
         }
     }
 }
