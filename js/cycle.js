83032fb7354617102fe389b59d16e42c4cc2aab6
zhangjian <zhangjian@xiaotong.com>
2018-04-21 13:19:53 +0800

    2.3

--- js/cycle.js
+++ js/cycle.js
@@ -3,15 +3,14 @@ var button=document.querySelectorAll('.in-button')[0];
 console.log(button);
 for(var i=0;i<divs.length;i++){
     button.onclick=function (ev) {
-        alert(1);
-        console.log(ev);
-        if(!ev.path[0].classList.contains('divs-active'))
+        var target=ev.target;
+        if(!target.classList.contains('divs-active'))
         {
             var active=document.querySelectorAll(".in-button");
             for(var j=0;j<active[0].children.length;j++){
                 active[0].children[j].classList.remove('divs-active');
             }
-            ev.path[0].classList.add('divs-active');
+            target.classList.add('divs-active');
         }
     }
 }
