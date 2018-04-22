1e026784839097d837a62b37258f1aa17d3d1098
zhangjian <zhangjian@xiaotong.com>
2018-04-22 14:57:17 +0800

    测试点击事件1

--- js/cycle.js
+++ js/cycle.js
@@ -1,15 +1,18 @@
-var divs=document.querySelectorAll(".in-button>input");
-for(var i=0;i<divs.length;i++){
-    divs[i].touchend=function (ev) {
-        alert(1);
-        console.log(ev.path[0].classList.contains('divs-active'));
-        if(!ev.path[0].classList.contains('divs-active'))
-        {
-            var active=document.querySelectorAll(".in-button");
-            for(var j=0;j<active[0].children.length;j++){
-                active[0].children[j].classList.remove('divs-active');
-            }
-            ev.path[0].classList.add('divs-active');
-        }
-    }
-}
+// var divs=document.querySelectorAll(".in-button>input");
+// for(var i=0;i<divs.length;i++){
+//     divs[i].touchend=function (ev) {
+//         alert(1);
+//         console.log(ev.path[0].classList.contains('divs-active'));
+//         if(!ev.path[0].classList.contains('divs-active'))
+//         {
+//             var active=document.querySelectorAll(".in-button");
+//             for(var j=0;j<active[0].children.length;j++){
+//                 active[0].children[j].classList.remove('divs-active');
+//             }
+//             ev.path[0].classList.add('divs-active');
+//         }
+//     }
+// }
+
+$('body').on('click', '.divs-active', function (e) {
+        alert('click');});
