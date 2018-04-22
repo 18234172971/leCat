45cefdd0d1663c0456fd5f8111a7de849c573a6a
zhangjian <zhangjian@xiaotong.com>
2018-04-22 23:02:16 +0800

    jquery点击事件测试1

--- js/cycle.js
+++ js/cycle.js
@@ -1,19 +1,3 @@
-// var divs=document.querySelectorAll(".in-button>input");
-// for(var i=0;i<divs.length;i++){
-//     divs[i].touchend=function (ev) {
-//         alert(1);
-//         console.log(ev.path[0].classList.contains('divs-active'));
-//         if(!ev.path[0].classList.contains('divs-active'))
-//         {
-//             var active=document.querySelectorAll(".in-button");
-//             for(var j=0;j<active[0].children.length;j++){
-//                 active[0].children[j].classList.remove('divs-active');
-//             }
-//             ev.path[0].classList.add('divs-active');
-//         }
-//     }
-// }
-
 $('.in-button>div').click(function () {
    $(this).siblings('.in-button>div').removeClass('divs-active');  // 删除其他兄弟元素的样式
     $(this).addClass('divs-active');
--- js/select.js
+++ js/select.js
@@ -1,15 +1,19 @@
-var texiao=document.querySelectorAll(".texiao");
-for(var i=0;i<texiao.length;i++){
-    console.log(texiao[i].classList);
-    if(!(texiao[i].classList==="texiaos")){
-        texiao[i].onclick=function (ev) {
-            console.log(ev);
-            var spans = document.querySelectorAll('.texiao');
-            for (var j = 0; j < spans.length; j++) {
-                spans[j].classList.remove('texiaos');
-            }
-            ev.path[0].classList.add('texiaos');
-        }
-    }
-
-}
+// var texiao=document.querySelectorAll(".texiao");
+// for(var i=0;i<texiao.length;i++){
+//     console.log(texiao[i].classList);
+//     if(!(texiao[i].classList==="texiaos")){
+//         texiao[i].onclick=function (ev) {
+//             console.log(ev);
+//             var spans = document.querySelectorAll('.texiao');
+//             for (var j = 0; j < spans.length; j++) {
+//                 spans[j].classList.remove('texiaos');
+//             }
+//             ev.path[0].classList.add('texiaos');
+//         }
+//     }
+//
+// }
+$('.texiao').click(function () {
+    $(this).siblings('.texiao').removeClass('texiaos');  // 删除其他兄弟元素的样式
+    $(this).addClass('texiaos');
+});
